d0fd4b3f511016f222e0d373663fb565f1efef2e
shokerm <moshe@shoker.co.il>
2024-09-08 18:12:57 +0300

    Add project files.

---
+++ WpfApplication-Ex/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApplication_Ex
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ WpfApplication-Ex/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApplication_Ex
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitializeComponent();
+            DispatcherTimer LiveTime = new DispatcherTimer();
+            LiveTime.Interval = TimeSpan.FromSeconds(1);
+            LiveTime.Tick += timer_Tick;
+            LiveTime.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            ClockText.Text = DateTime.Now.ToString("HH:mm:ss");
+            ClockDate.Text = DateTime.Now.ToString("d");
+        }
+
+        private void Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image image = (sender as Image)!;
+            image.Opacity = 0.6;
+            
+           
+        }
+      
+
+        private void Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Image)!.Opacity = 1;
+        }
+
+        private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Name: User\nEmail: user@gmail.com\nPhone: 0500000000", "👤Contact Information");
+        }
+
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Border).BorderBrush = Brushes.AliceBlue;
+
+
+        }
+
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Border).BorderBrush = Brushes.Transparent;
+        }
+    }
+}
